e2e49c45e3d540ba101d09d45e4e8e9be7d68bc4
hchiam <hchiam@gmail.com>
2020-04-02 23:29:19 -0400

    add example input and output

---
+++ input.js
@@ -0,0 +1,7 @@
+alertValue('some value');
+
+function alertValue(value) {
+  const someVariable = 1;
+  let anotherVariable = someVariable + 2;
+  alert(`value is: ${value} and anotherVariable is: ${anotherVariable}`);
+}
---
+++ output.js
@@ -0,0 +1,4 @@
+function alertValue(a) {
+  alert(`value is: ${a} and anotherVariable is: 3`);
+}
+alertValue("some value");

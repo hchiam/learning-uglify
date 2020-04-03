a3bda074caf173c66a1193fb914e82ef7b8faa82
hchiam <hchiam@gmail.com>
2020-04-02 23:38:08 -0400

    actual output

--- output.js
+++ output.js
@@ -1,4 +1,1 @@
-function alertValue(a) {
-  alert(`value is: ${a} and anotherVariable is: 3`);
-}
-alertValue("some value");
+function alertValue(a){alert(`value is: ${a} and anotherVariable is: 3`)}alertValue("some value");
